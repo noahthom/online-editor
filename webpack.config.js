40fd55f7c5d83be16a71c43d146cc6ac9df66218
Noah Rozario <noahrozario@Noahs-MacBook-Pro.local>
2022-03-18 09:53:49 -0600

    made changes to config for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        static: path.join(__dirname,'public')
+module.exports= (env, argv) => {
+    const isProduction = argv.mode === 'production'
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname,'public'),
+            filename: 'bundle.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }]
+            }]
+        },
+        plugins: [new MiniCssExtractPlugin({ filename: 'styles.css'})],
+        devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+        devServer: {
+            static: path.join(__dirname,'public')
+        }
     }
 }
